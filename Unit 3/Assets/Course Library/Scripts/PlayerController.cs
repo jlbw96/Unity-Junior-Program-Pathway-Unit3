8b2b1c88f55cf389f17e89d47cb7fb71cf2c0ad0
jlbw96 <jlbw96@hotmail.co.uk>
2021-02-25 19:35:48 +0000

    Lesson 3.1 - Jump Force
    Finished 3.1

---
+++ Unit 3/Assets/Course Library/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //Declare rigidbody
+    private Rigidbody playerRb;
+    public float jumpForce;
+    public float gravityModifier;
+    //Grounded Check
+    public bool isOnGround = true; 
+
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //If (spacebar is pressed and boolean isOnGround is currently true)
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            //Add force to the rigidbody and set boolean to false;
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //If box collider collides with an object then boolean is set to true;
+        isOnGround = true;
+    }
+
+
+}
---
+++ Unit 3/Assets/Course Library/Scripts/SpawnManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    //Declare gameobject that we can assign to
+    public GameObject obstaclePrefab;
+    //spawn location for object
+    private Vector3 spawnPos = new Vector3(25, 0, 0);
+    //floats for object spawn delay and repeat rate
+    private float startDelay = 2;
+    private float repeatRate = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Call the SpawnObstacle function repeatedly based on the delay and repeat rate
+        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+    }
+
+    void SpawnObstacle()
+    {
+        //Instantiate the gameobject we are using at the position declared above
+        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+    }
+    
+}
