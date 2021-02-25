0f9b553a39bbb96a21239b54ece16054f2af3be6
jlbw96 <jlbw96@hotmail.co.uk>
2021-02-25 21:24:26 +0000

    Lesson 3.3 - Don't Just Stand There
    Lesson 3.3 completed.

--- Unit 3/Assets/Course Library/Scripts/PlayerController.cs
+++ Unit 3/Assets/Course Library/Scripts/PlayerController.cs
@@ -7,9 +7,11 @@ public class PlayerController : MonoBehaviour
 
     //Declare rigidbody
     private Rigidbody playerRb;
+    //Declare a new animator
+    private Animator playerAnim;
 
-    public float jumpForce;
-    public float gravityModifier;
+    public float jumpForce = 10;
+    public float gravityModifier = 1;
 
     //Grounded Check
     public bool isOnGround = true;
@@ -20,18 +22,21 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
         Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //If (spacebar is pressed and boolean isOnGround is currently true)
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        //If (spacebar is pressed and boolean isOnGround is true and gameover is false)
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
         {
             //Add force to the rigidbody and set boolean to false;
+            //Change animation to jump
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
+            playerAnim.SetTrigger("Jump_trig");
         }
     }
 
@@ -42,11 +47,14 @@ public class PlayerController : MonoBehaviour
         {
             isOnGround = true;
         }
-        //Else if collision with object with obstacle tag display debug text game over and set gameover boolean to true
+        //Else if collision with object with obstacle tag display debug text game over
+        //set gameover boolean to true, set death animation to true and set int to 1
         else if (collision.gameObject.CompareTag("Obstacle"))
         {
             gameOver = true;
             Debug.Log("Game Over!");
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
         }
     }
 
