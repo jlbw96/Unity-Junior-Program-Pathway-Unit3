d7757ee4afbec669315c67d726a3ec6da5ce5f8f
jlbw96 <jlbw96@hotmail.co.uk>
2021-03-02 16:40:26 +0000

    Lab 3 - Player Control Start
    Started Lab 3

---
+++ Lab 3 - Player Control/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //Player movement speed
+    public float speed = 18.0f;
+
+    //Player rigidbody
+    private Rigidbody playerRb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Get rigidbody from player
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+    }
+
+    void MovePlayer()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        playerRb.AddForce(Vector3.forward * speed * verticalInput);
+        playerRb.AddForce(Vector3.right * speed * horizontalInput);
+    }
+}
